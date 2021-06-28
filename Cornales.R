de86337df02211d2a1f857ad26275984417d4df2
Joel Santos <joeljesussantos50@gmail.com>
2021-06-28 21:01:17 +0100

    Added the 'Cornales.R' file with the PCA analysis.

---
+++ Cornales.R
@@ -0,0 +1,36 @@
+library(pcaMethods)
+geno_data = read.csv("~/Documents/ASB/assignment_02/Cornales.csv",
+                    header=TRUE,
+                    row.names=1,
+                    sep=" ",
+                    na.strings=9)
+
+summary(geno_data)
+geno_data = geno_data[rowSums(is.na(geno_data)) != ncol(geno_data)-1, ]
+
+
+View(geno_data)
+
+
+colors = factor(geno_data$Species)
+        
+tlep_pca = pca(geno_data[,-c(1)],
+               scale="none",
+               center=T,
+               nPcs=2,
+               method="nipals")
+# We have to use the `nipals` method because we have missing data
+
+print(tlep_pca@R2)
+
+# Use the age group as discriminant
+slplot(tlep_pca,
+       scol=colors,
+       scoresLoadings=c(TRUE,FALSE),
+       sl=NULL,  # What does this do?
+       spch="x")
+
+legend("topright",
+       legend=unique(colors), # sort(unique(geno_data[, "Species"])),
+       col=unique(colors),
+       pch="x")
